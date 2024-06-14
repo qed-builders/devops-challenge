8e179ef610093337ec9f0e271ec7e2ba085c168c
andrei113 <andrei@weareuncapped.com>
2024-06-14 20:30:30 +0300

    fixing yarn lint

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {  // eslint-disable-line no-unused-vars
   res.send('respond with a resource');
 });
 
